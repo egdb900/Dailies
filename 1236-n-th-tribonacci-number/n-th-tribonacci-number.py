df51284eac447b4fe3da836d841d643e8a85e1fb
Elijah Bayani <114788174+egdb900@users.noreply.github.com>
2024-04-17 08:44:09 -0700

    Time: 14 ms (51.88%) | Memory: 11.4 MB (94.51%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -5,7 +5,6 @@ class Solution(object):
         :rtype: int
         """
         T = [0, 1, 1]
-
         for i in range(3, n+1):
             T.append(T[i-3] + T[i-2] + T[i-1])
         return T[n]
