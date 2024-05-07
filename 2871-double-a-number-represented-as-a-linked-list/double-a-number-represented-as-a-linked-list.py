c913290673b6daefd01973992b1ebce9d47ab2a8
Elijah Bayani <114788174+egdb900@users.noreply.github.com>
2024-05-07 16:24:36 -0700

    Time: 385 ms (95.56%) | Memory: 19.2 MB (71.11%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -10,17 +10,13 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         prev, curr = None, head
-        curr.val *= 2
-        if curr.val > 9:
-            curr.val %= 10
-            prev = ListNode(1, curr)
-            head = prev
-        prev = curr
-        curr = curr.next
         while curr:
             curr.val *= 2
             if curr.val > 9:
-                curr.val %= 10
+                if prev == None:
+                    prev = ListNode(0, curr)
+                    head = prev
+                curr.val -= 10
                 prev.val += 1
             prev = curr
             curr = curr.next
